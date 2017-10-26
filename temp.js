07b85e291795978eb75bfa2ddfdaadbed7d468e5
Alexander Avramenko <alexander.avramenko@netent.com>
2017-10-26 16:49:00 +0300

    update

--- temp.js
+++ temp.js
@@ -1,15 +1,3 @@
-var object1 = {
-    obj: {
-        number: 0
-    }
-};
-
-var object2 = {
-    str: "Kolya"
-};
-
-object2.str.number = object2.str.number;
-
-object2.str.number++;
-
-console.log(object2.str.number);
+var r = -9;
+var t = -8;
+console.log(r == t);
