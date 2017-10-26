b7a000bdc4fe87eb1d82a0e4d4bb71572228e35a
Alexander Avramenko <alexander.avramenko@netent.com>
2017-10-26 15:11:25 +0300

    add new files

---
+++ temp.js
@@ -0,0 +1,15 @@
+var object1 = {
+    obj: {
+        number: 0
+    }
+};
+
+var object2 = {
+    str: "Kolya"
+};
+
+object2.str.number = object2.str.number;
+
+object2.str.number++;
+
+console.log(object2.str.number);
