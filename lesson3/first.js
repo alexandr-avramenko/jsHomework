66f59f482f71f4d4cf6fc08e72a6b63cfbeb7f8c
Alexander Avramenko <alexander.avramenko@netent.com>
2017-10-31 16:19:52 +0200

    add new one "3"

---
+++ lesson3/first.js
@@ -0,0 +1,13 @@
+//TASK #1
+var a = prompt("Enter a number:", '0');
+if (a === 0) {
+    console.log("Right")
+} else {
+    console.log("Wrong");
+}
+/*
+1 - 'Wrong'
+0 - 'Right'
+-3 - 'Wrong
+ */
+
