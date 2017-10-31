e7ae046f6efef8987c5b36464750b9b6f14769ce
Alexander Avramenko <alexander.avramenko@netent.com>
2017-10-31 16:33:42 +0200

    update 3

--- lesson3/first.js
+++ lesson3/first.js
@@ -1,5 +1,5 @@
 //TASK #1
-var a = prompt("Enter a number:", '0');
+var a = +prompt("Enter a number", '0');
 if (a === 0) {
     console.log("Right")
 } else {
@@ -8,6 +8,33 @@ if (a === 0) {
 /*
 1 - 'Wrong'
 0 - 'Right'
--3 - 'Wrong
+-3 - 'Wrong'
  */
 
+//TASK #2
+var x = 'test';
+if (x === 'test') {
+    console.log("Right");
+} else {
+    console.log('Wrong');
+}
+/*
+'test' - 'Right'
+'тест' - 'Wrong'
+3 - 'Wrong'
+ */
+
+//TASK #3
+// variant_1
+var test = 1;
+if (test == true) {
+    console.log("Right");
+} else {
+    console.log("Wrong");
+}
+// variant_2
+test == true ? console.log("Right") : console.log("Wrong");
+/*
+true - 'Right'
+false - 'Wrong'
+ */
