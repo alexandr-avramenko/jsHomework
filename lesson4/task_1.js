a0ad0ba954c1330c0417783006c8914d88a073ff
Alex Avramenko <avramenko423@gmail.com>
2017-11-04 23:21:08 +0200

    add 4(1)

---
+++ lesson4/task_1.js
@@ -0,0 +1,27 @@
+//----------------------- TASK #1 -----------------------
+function getPrimesSumBelow(from, to) {
+    var arr = [];
+    for (var i = to; i >= from; i--) {
+        if (primeNums(i)) {
+            arr.push(i);
+        }
+    }
+    return arr.reverse();
+}
+
+getPrimesSumBelow(2, 10);
+
+function primeNums (x) {
+    if ( x !== 1 && x <= 3 ) {
+        return 1;
+    } else {
+        for (var i = 2; i < x; i++) {
+            if (x % i === 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+//----------------------- TASK #2 -----------------------
